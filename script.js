cb65990991ba7bd6b9aac2a31ee5f4a4090e84b9
EverSinceWWI <32314108+EverSinceWWI@users.noreply.github.com>
2019-05-16 11:22:46 +0200

    sync

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-var colors = ['#107db6','#e8aa15','#ae2956','#734396','#6ebeb9','#ed868c','#76b12c','#e26d22','#723c2c'];
+var  services = {
+  0: {
+    "name":"Birth Declarations",
+    "letter":"B",
+    "color":"#734396",
+  },
+  1: {
+    "name":"Civil Unions",
+    "letter":"U",
+    "color":"#6ebeb9",
+  },
+  2: {
+    "name":"Divorces",
+    "letter":"D",
+    "color":"#ed868c",
+  },
+  3: {
+    "name":"ID Card",
+    "letter":"L",
+    "color":"#76b12c",
+  },
+  4: {
+    "name":"Certificates",
+    "letter":"C",
+    "color":"#e26d22",
+  },
+  5: {
+    "name":"Electoral Card",
+    "letter":"E",
+    "color":"#723c2c",
+  },
+  6: {
+    "name":"Emigration",
+    "letter":"M",
+    "color":"#107db6",
+  },
+  7: {
+    "name":"Citizenships",
+    "letter":"T",
+    "color":"#ae2956",
+  },
+  8: {
+    "name":"Address Change",
+    "letter":"A",
+    "color":"#e8aa15",
+  },
+}
 
 function bodyOnLoad() {
   var randomCol = Math.floor(Math.random() * (8-0) + 0);
   var randomTicket = Math.floor(Math.random() * (6-124) + 124);
   var randomQueue = Math.floor(Math.random() * (3-42) + 42);
+  var randomT = Math.floor(Math.random() * (8-0) + 0);
 
-   document.body.style.backgroundColor = colors[randomCol];
-   document.getElementById("num").innerHTML="L"+randomTicket;
+   document.body.style.backgroundColor = services[randomT].color;
+   document.getElementById("num").innerHTML=services[randomT].letter+randomTicket;
+   document.getElementById("ticketType").innerHTML=services[randomT].name.toUpperCase();
    document.getElementById("ticketRemaining").innerHTML=randomQueue+" people remaining";
 }
